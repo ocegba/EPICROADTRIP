d2963775c7c6772aeb653f2332490892f8c62b85
ocegba <oceane.gbamene@epitech.eu>
2023-02-28 16:09:24 +0100

    debut test

---
+++ frontend/src/__tests__/AdminPage.test.js
@@ -0,0 +1,42 @@
+import userEvent from "@testing-library/user-event";
+import { render, screen, debug, getByAltText } from "@testing-library/react";
+import AdminPage from '../pages/AdminPage';
+import { act } from "react-dom/test-utils";
+
+test('renders Admin page ====> Adhérents', () => {
+  render(<AdminPage />);
+
+  const itnLink = screen.getByRole("button", { name: /Mes adhérents/ });
+  act(() => {
+    userEvent.click(itnLink);
+  });
+
+  const heading1 = screen.getByRole('heading', { name: /Mes adhérents/i, level: 1 });
+  const heading3 = screen.getByRole('heading',  {name : /Dans cette section, vous pouvez supprimer les comptes des adhérents/i, level : 3})
+
+  expect(heading1).toBeInTheDocument();
+  expect(heading3).toBeInTheDocument();
+});
+
+test('renders Admin page ====> Les itinéraires', () => {
+    render(<AdminPage />);
+
+    const regLink = screen.getByRole("button", { name: /Les itinéraires/ });
+    act(() => {
+      userEvent.click(regLink);
+    });
+
+    const heading1 = screen.getByRole('heading', { name: /Les itinéraires/i, level: 1 });
+    const heading3 = screen.getByRole('heading',  {name : /Dans cette section, vous pouvez supprimer les itinéraires publiés/i, level : 3})
+
+
+    expect(heading1).toBeInTheDocument();
+    expect(heading3).toBeInTheDocument();
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(usrInput).toBeInTheDocument();
+
+    expect(mdfButton).toBeInTheDocument();
+    expect(dltButton).toBeInTheDocument();
+  });
--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -1,12 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function AdminPage() {
+function ItnFct() {
   return (
     <div>
-      <h1>AdminPage</h1>
-      <p>Bienvenue sur AdminPage</p>
+      <h1>Mes itinéraires</h1>
+      <h3>Dans cette section, vous pouvez supprimer, imprimer ou publier les itinéraires</h3>
     </div>
   );
 }
 
-export default AdminPage;
+function Reglages() {
+  return (
+    <div>
+      <h1>Mes réglages</h1>
+      <h3>Dans cette section, vous pouvez éditer votre profil</h3>
+
+      <div>
+      <label id="username-label">Username</label>
+        <input aria-labelledby="username-label" />
+
+        <label id="email-label">Email</label>
+        <input aria-labelledby="email-label" />
+
+        <label id="password-label">Mot de passe</label>
+        <input aria-labelledby="password-label" />
+
+        <button>Modifier</button>
+
+        <button>Supprimer mon compte</button>
+      </div>
+    </div>
+  );
+}
+
+function Profil() {
+  const [itnOpen, itnIsOpen] = useState(false);
+  const [regOpen, regIsOpen] = useState(false);
+
+  return (
+    <div>
+      <div className="SideBar">
+        <div>
+          <button
+            onClick={() => {
+              itnIsOpen(true);
+              regIsOpen(false);
+            }}
+          >
+            Mes itinéraires
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              itnIsOpen(false);
+              regIsOpen(true);
+            }}
+          >
+            Mes réglages
+          </button>
+        </div>
+      </div>
+      {regOpen && !itnOpen ? <Reglages /> : <ItnFct />}
+    </div>
+  );
+}
+
+export default Profil;
