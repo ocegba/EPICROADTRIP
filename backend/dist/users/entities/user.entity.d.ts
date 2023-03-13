cbe22e9957b50826a18999aed1854ef388128644
Gautier Pamies <gautier.pamies34@gmail.com>
2023-03-13 16:46:36 +0100

    feat(dist): Removed backend dist directory

--- backend/dist/users/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    userId: number;
-    id: number;
-    Username: string;
-    Email: string;
-    Password: string;
-    RoleId: string;
-    Created_at: Date;
-}
--- backend/dist/users/users.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { User } from './entities/user.entity';
-export declare class UsersService {
-    private users;
-    findByEmail(Email: string): Promise<User | undefined>;
-    findOne(id: number): Promise<User | undefined>;
-}
