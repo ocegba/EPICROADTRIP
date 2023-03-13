7ea9eed43c0e644bdcbf07a014d6aac6f1137a09
Olivier <olivier.vic@epitech.eu>
2023-03-13 10:58:58 +0100

    test typeorm

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    Id: string;
+
+    @Column()
+    Username: string;
+
+    @Column()
+    Email: string;
+
+    @Column()
+    Password: string;
+
+    @Column()
+    RoleId: string;
+
+    @Column()
+    Created_at: Date;
+
+}
