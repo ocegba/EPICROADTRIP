f4e175b97b94357b490238a43e008a92e0473e22
Gautier Pamies <gautier.pamies34@gmail.com>
2023-03-13 10:26:39 +0100

    token implement

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -13,8 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.ACCESS_SECRET,
-      // secretOrKey: '52FE5615F877C573',
+      secretOrPrivateKey: process.env.ACCESS_SECRET,
     });
   }
 
