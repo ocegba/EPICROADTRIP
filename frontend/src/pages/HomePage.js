437d822b32e8eb685980592b644cec3ae243a670
ocegba <oceane.gbamene@epitech.eu>
2023-03-22 19:02:14 +0100

    add css

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+
 import peacevalleyImg from "../media/PeaceValley.jpg";
 import airplaneImg from "../media/Airplane.png";
 import mapImg from "../media/Map.png";
+
+import Icon from "@mui/material/Icon";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 
 function affichMap() {
   return (
@@ -16,22 +20,36 @@ function HomePage() {
   const [mpOpen, mpIsOpen] = useState(false);
   return (
     <div>
-      {!mpOpen ? <div title="srchPlc">
-        <img src={peacevalleyImg} alt="Vallée Atmosphérique"></img>
-        <h1>Bienvenue sur Epic Road Trip, Planifiez vos voyages où découvrez ceux des autres !</h1>
-        <input type="search" placeholder="Où voulez-vous aller ?"></input>
-        <button on onClick={() => {
-          mpIsOpen(true);
-        }}>Valider</button>
-      </div> : affichMap() }
+      {!mpOpen ? (
+        <div title="srchPlc">
+          <img src={peacevalleyImg} alt="Vallée Atmosphérique"></img>
+          <h1>
+            Bienvenue sur Epic Road Trip, Planifiez vos voyages où découvrez
+            ceux des autres !
+          </h1>
+          <input type="search" placeholder="Où voulez-vous aller ?"></input>
+          <button
+            on
+            onClick={() => {
+              mpIsOpen(true);
+            }}
+          >
+            Valider
+          </button>
+        </div>
+      ) : (
+        affichMap()
+      )}
       <div>
-        <img src={airplaneImg} title="trvlIcon" />
-        <h3>Voyage</h3>
+        <div className="travel-entete">
+          <Icon aria-label="travel">
+            <FlightTakeoffIcon />
+          </Icon>
+          <h3>Voyage</h3>
+        </div>
       </div>
     </div>
   );
 }
 
 export default HomePage;
-
-
