2fdc3f9f092671830bc7c85e4e51012be03e7790
ocegba <oceane.gbamene@epitech.eu>
2023-03-22 19:01:37 +0100

    add css

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,44 +4,54 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logout } from "../services/auth";
 
-import Button from '@mui/material/Button';
+import logo from "../media/logo.png";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from "@mui/icons-material/Logout";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="Navbar">
-        <div>
-          <div>
-            <Link to="/">
-              <img src="logo.png" alt="Logo" />
+        <div className="logo">
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+          <h1>Les Oui-Vie</h1>
+          <li>
+            <Link name="Itinéraires" to="/itineraires">
+              Itinéraires
             </Link>
-          </div>
-          <div>
-            <ul>
-              <li>
-                <Link name="Itinéraires" to="/itineraires">
-                  Itinéraires
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div>
+          </li>
+        </div>
+        <div>
+          <ul>
             {this.props.isAuthUser ? (
               <>
-                <Link to="/profil">
-                  <img src="profil.png" alt="Profil" />
-                </Link>
-                <Button color="inherit" onClick={this.props.logout}>
-                  Logout
-                </Button>
+                <li>
+                  <Link to="/profil">
+                    <Avatar src="/static/images/avatar/1.jpg" />
+                  </Link>
+                </li>
+                <li>
+                  <IconButton aria-label="delete"  onClick={this.props.logout}>
+                    <LogoutIcon />
+                  </IconButton>
+                </li>
               </>
             ) : (
               <>
-                <Link to="/register">Register</Link>
-                <Link to="/login">Login</Link>
+                <li>
+                  <Link to="/register">Inscription</Link>
+                </li>
+                <li>
+                  <Link to="/login">Connexion</Link>
+                </li>
               </>
             )}
-          </div>
+          </ul>
         </div>
       </nav>
     );
