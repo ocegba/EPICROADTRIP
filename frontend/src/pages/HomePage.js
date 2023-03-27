2f6809c79aef60889f7376f9a488f9d3d2680f27
ocegba <oceane.gbamene@epitech.eu>
2023-03-27 15:43:50 +0200

    fix GMap

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,40 +1,49 @@
 import React, { useState } from "react";
 import peacevalleyImg from "../media/PeaceValley.jpg";
 import airplaneImg from "../media/Airplane.png";
-import GMap from "../components/GMap"
-import {Autocomplete} from '@react-google-maps/api';
-
-
-
-function affichMap(location) {
-  const map = GMap(document.getElementById('map'));
-  map.setCenter()
-  return (
-    <div title="srchMap">
-      <button>burger</button>
-      <GMap id="map" />
-    </div>  
-  );
-}
+import GMap from "../components/GMap";
+import { Autocomplete } from "@react-google-maps/api";
 
 function HomePage() {
   const [mpOpen, mpIsOpen] = useState(false);
   const [autocomplete, setAutocomplete] = useState(null);
-  const handlePlaceSelect= (place) =>{console.log(place)};
+  const [valueLocation, setValueLocation] = useState("");
+
   return (
     <div>
-      {!mpOpen ? <div title="srchPlc">
-        <img src={peacevalleyImg} alt="Vallée Atmosphérique"></img>
-        <h1>Bienvenue sur Epic Road Trip, Planifiez vos voyages où découvrez ceux des autres !</h1>
-        {/* <input type="search" placeholder="Où voulez-vous aller ?"></input> */}
-        <Autocomplete onLoad = {(autocomplete) => setAutocomplete(autocomplete)}
-        onPlaceChanged ={() => handlePlaceSelect(autocomplete.getPlace())}>
-            <input id ="txtqry" type="text" placeholder="Où voulez-vous aller ?"/>
-            </Autocomplete>
-        <button onClick={() => {
-          mpIsOpen(true);
-        }}>Valider</button>
-      </div> : affichMap(document.getElementById('txtqry').value)}
+      {mpOpen ? (
+        <div title="srchPlc">
+          <img src={peacevalleyImg} alt="Vallée Atmosphérique"></img>
+          <h1>
+            Bienvenue sur Epic Road Trip, Planifiez vos voyages où découvrez
+            ceux des autres !
+          </h1>
+          {/* <input type="search" placeholder="Où voulez-vous aller ?"></input> */}
+
+          <Autocomplete
+            onLoad={(autocomplete) => setAutocomplete(autocomplete)}
+            onPlaceChanged={() => {
+              setValueLocation(autocomplete.getPlace());
+            }}
+          >
+            <input
+              id="txtqry"
+              type="text"
+              placeholder="Où voulez-vous aller ?"
+            />
+          </Autocomplete>
+          <button
+            onClick={() => {
+              mpIsOpen(true);
+            }}
+          >
+            Valider
+          </button>
+        </div>
+      ) : (
+        <GMap location={valueLocation} />
+      )}
+
       <div>
         <img src={airplaneImg} title="trvlIcon" />
         <h3>Voyage</h3>
@@ -44,5 +53,3 @@ function HomePage() {
 }
 
 export default HomePage;
-
-
