80de6619c203bfa8883cf737eb338c3b02285234
Baptiste Lavier <d4ngeur@gmail.com>
2023-04-03 16:56:56 +0200

    getCoord working

--- frontend/src/components/GMap.js
+++ frontend/src/components/GMap.js
@@ -1,28 +1,49 @@
-import React,  {useState} from 'react'
-import {GoogleMap} from '@react-google-maps/api';
+import React, { useState, useEffect } from "react";
+import { GoogleMap } from "@react-google-maps/api";
 
 const containerStyle = {
-  width: '1080px',
-  height: '920px'
+  width: "1080px",
+  height: "920px",
 };
 
 const initialCoordinates = {
   lat: -3.745,
-  lng: -38.523
+  lng: -38.523,
 };
 
-function GMap(){
-    const [mapCenter, setMapCenter] = useState(initialCoordinates);
-    const [mapZoom, setMapZoom] = useState(16);
-    return(
+function GMap({ location, onGMValue }) {
+  const [mapCenter, setMapCenter] = useState(initialCoordinates);
+  const [mapZoom, setMapZoom] = useState(16);
+  const [coordinates, setCoordinates] = useState({});
 
-        <GoogleMap
-        mapContainerStyle ={containerStyle}
-        center = {mapCenter}
-        zoom = {mapZoom}
-        >
-        </GoogleMap>
-    )
+  function clickOnMap(e) {
+    const cLat = e.latLng.lat();
+    const cLng = e.latLng.lng();
+    setCoordinates({
+      lat: cLat,
+      lng: cLng,
+    });
+    onGMValue(coordinates);
+  }
+
+  useEffect(() => {
+    if (location) {
+      setMapCenter({
+        lat: location.geometry.location.lat(),
+        lng: location.geometry.location.lng(),
+      });
+      setMapZoom(10);
+    }
+  }, [location]);
+
+  return (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={mapCenter}
+      zoom={mapZoom}
+      onClick={(e) => clickOnMap(e)}
+    ></GoogleMap>
+  );
 }
 
 export default GMap;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,48 +1,70 @@
 import React, { useState } from "react";
 import peacevalleyImg from "../media/PeaceValley.jpg";
-import airplaneImg from "../media/Airplane.png";
-import GMap from "../components/GMap"
-import {Autocomplete} from '@react-google-maps/api';
+import GMap from "../components/GMap";
+import { Autocomplete } from "@react-google-maps/api";
+import mapImg from "../media/Map.png";
 
-
-
-function affichMap(location) {
-  const map = GMap(document.getElementById('map'));
-  map.setCenter()
-  return (
-    <div title="srchMap">
-      <button>burger</button>
-      <GMap id="map" />
-    </div>  
-  );
-}
+import Icon from "@mui/material/Icon";
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 
 function HomePage() {
   const [mpOpen, mpIsOpen] = useState(false);
   const [autocomplete, setAutocomplete] = useState(null);
-  const handlePlaceSelect= (place) =>{console.log(place)};
+  const [valueLocation, setValueLocation] = useState("");
+  const [GMcoordinates, setGMCoordinates] = useState(null);
+  const handleGMValue = (coordinates) => {
+    setGMCoordinates(coordinates);
+  };
+
   return (
     <div>
-      {!mpOpen ? <div title="srchPlc">
-        <img src={peacevalleyImg} alt="Vallée Atmosphérique"></img>
-        <h1>Bienvenue sur Epic Road Trip, Planifiez vos voyages où découvrez ceux des autres !</h1>
-        {/* <input type="search" placeholder="Où voulez-vous aller ?"></input> */}
-        <Autocomplete onLoad = {(autocomplete) => setAutocomplete(autocomplete)}
-        onPlaceChanged ={() => handlePlaceSelect(autocomplete.getPlace())}>
-            <input id ="txtqry" type="text" placeholder="Où voulez-vous aller ?"/>
-            </Autocomplete>
-        <button onClick={() => {
-          mpIsOpen(true);
-        }}>Valider</button>
-      </div> : affichMap(document.getElementById('txtqry').value)}
+      {!mpOpen ? (
+        <div title="srchPlc">
+          <img src={peacevalleyImg} alt="Vallée Atmosphérique"></img>
+          <h1>
+            Bienvenue sur Epic Road Trip, Planifiez vos voyages où découvrez
+            ceux des autres !
+          </h1>
+          {/* <input type="search" placeholder="Où voulez-vous aller ?"></input> */}
+
+          <Autocomplete
+            onLoad={(autocomplete) => setAutocomplete(autocomplete)}
+            onPlaceChanged={() => {
+              setValueLocation(autocomplete.getPlace());
+            }}
+          >
+            <input
+              id="txtqry"
+              type="text"
+              placeholder="Où voulez-vous aller ?"
+            />
+          </Autocomplete>
+          <button onClick={() => mpIsOpen(true)}>Valider</button>
+        </div>
+      ) : (
+        <div title="srchMap">
+          <button>burger</button>
+          {/* <img src={mapImg} alt="Map"></img> */}
+          <GMap location={valueLocation} onGMValue={handleGMValue} />
+          <p>
+            Coordinates:{" "}
+            {GMcoordinates
+              ? `${GMcoordinates.lat}, ${GMcoordinates.lng}`
+              : "Unknown"}
+          </p>
+        </div>
+      )}
+
       <div>
-        <img src={airplaneImg} title="trvlIcon" />
-        <h3>Voyage</h3>
+        <div className="travel-entete">
+          <Icon aria-label="travel">
+            <FlightTakeoffIcon />
+          </Icon>
+          <h3>Voyage</h3>
+        </div>
       </div>
     </div>
   );
 }
 
 export default HomePage;
-
-
