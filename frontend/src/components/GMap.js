daf146b4c1c06636dfe1d28af31dce1bda7886ba
Baptiste Lavier <d4ngeur@gmail.com>
2023-04-05 16:21:32 +0200

    tests infructueux du burger menu pls kill me

--- frontend/src/components/GMap.js
+++ frontend/src/components/GMap.js
@@ -11,10 +11,13 @@ const initialCoordinates = {
   lng: -38.523,
 };
 
+
+
 function GMap({ location, onGMValue }) {
   const [mapCenter, setMapCenter] = useState(initialCoordinates);
   const [mapZoom, setMapZoom] = useState(16);
   const [coordinates, setCoordinates] = useState({});
+  const [mapStyles, setMapStyles] = useState([]);
 
   function clickOnMap(e) {
     const cLat = e.latLng.lat();
@@ -24,17 +27,26 @@ function GMap({ location, onGMValue }) {
       lng: cLng,
     });
     onGMValue(coordinates);
-  }
-
+  };
   useEffect(() => {
     if (location) {
       setMapCenter({
         lat: location.geometry.location.lat(),
         lng: location.geometry.location.lng(),
       });
-      setMapZoom(10);
+      setMapZoom(10)};
+      setMapStyles([      {        
+        featureType: "poi",        
+        stylers: [{ visibility: "off" }],
+        },
+        {
+          featureType: "transit",
+          stylers: [{ visibility: "off" }],
+        }
+      ]);
     }
-  }, [location]);
+  , [location]);
+  
 
   return (
     <GoogleMap
@@ -42,6 +54,7 @@ function GMap({ location, onGMValue }) {
       center={mapCenter}
       zoom={mapZoom}
       onClick={(e) => clickOnMap(e)}
+      options ={{styles:mapStyles}}
     ></GoogleMap>
   );
 }
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,13 +1,10 @@
-import React, { useState } from "react";
-
+import React, { useState, useRef } from "react";
 import peacevalleyImg from "../media/PeaceValley.jpg";
 import GMap from "../components/GMap";
-import { Autocomplete } from "@react-google-maps/api";
+import { Autocomplete, GoogleMap } from "@react-google-maps/api";
 import mapImg from "../media/Map.png";
-
 import Icon from "@mui/material/Icon";
 import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
-
 function HomePage() {
   const [mpOpen, mpIsOpen] = useState(false);
   const [autocomplete, setAutocomplete] = useState(null);
@@ -16,7 +13,91 @@ function HomePage() {
   const handleGMValue = (coordinates) => {
     setGMCoordinates(coordinates);
   };
+  let isRest = false;
+  let isDiv = false;
+  let isTransp =false;
 
+  /*function changeMapDisplay(Type) {
+    console.log(Type)
+    console.log(isRest)
+    switch (Type) {
+      case "Restaurants" :
+        if (isRest == false){
+          isRest = true;
+          mapRefGmap.setOptions({styles:[  
+            {
+              "featureType": "poi.buisness",
+              "stylers": [
+                {
+                  "visibility": "on"
+                }
+              ]
+            }]})}
+        else
+        {
+          isRest = false;
+          document.getElementById("mapVr").setOptions({styles:[  
+            {
+              "featureType": "poi.buisness",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            }]})
+        }
+      case "Divertissements" :
+        if (isDiv == false){
+          isDiv = true;
+          document.getElementById("mapVr").setOptions({styles:[  
+          {
+            "featureType": "poi.attraction",
+            "stylers": [
+              {
+                "visibility": "on"
+              }
+            ]
+          }]})}
+        else{
+          isDiv = false;
+          document.getElementById("mapVr").GMap.setOptions({styles:[  
+            {
+              "featureType": "poi.attraction",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            }]})
+        }
+        case "Transports" :
+          if (isTransp == false){
+            isTransp = true;
+            document.getElementById("mapVr").setOptions({styles:[  
+              {
+                "featureType": "transit",
+                "stylers": [
+                  {
+                    "visibility": "on"
+                  }
+                ]
+          }]})}
+          else {
+            isTransp =false;
+            document.getElementById("mapVr").setOptions({styles:[  
+              {
+                "featureType": "transit",
+                "stylers": [
+                  {
+                    "visibility": "off"
+                  }
+                ]
+              }]})
+          }
+        }
+      }*/
+    
+  
   return (
     <div>
       {!mpOpen ? (
@@ -45,6 +126,25 @@ function HomePage() {
       ) : (
         <div title="srchMap">
           <button>burger</button>
+          <fieldset title ="brgrMenu">
+            <Autocomplete
+              onLoad={(autocomplete) => setAutocomplete(autocomplete)}
+              onPlaceChanged={() => {
+                setValueLocation(autocomplete.getPlace());
+              }}
+            >
+              <input
+                id="brgTxtQry"
+                type="text"
+              />
+            </Autocomplete>
+            <label for = "Restaurants">Restaurants et Hotels</label>
+            <input type="checkbox" id="Restaurants" title="Restaurants" name="Restaurants"></input>
+            <label for = "Divertissements">Divertissements</label>
+            <input type="checkbox" id="Divertissements" title="Divertissements" name="Divertissements" ></input>
+            <label for = "Tranports">Transports</label>
+            <input type="checkbox" id="Transports" title="Transports" name="Transports" ></input>
+          </fieldset>
           {/* <img src={mapImg} alt="Map"></img> */}
           <GMap location={valueLocation} onGMValue={handleGMValue} />
           <p>
