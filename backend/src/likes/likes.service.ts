f1140f5ab3750ff7db3fbebd8f9c0e0bb72339d9
ocegba <oceane.gbamene@epitech.eu>
2023-04-07 15:17:49 +0200

    add dependency User

--- backend/src/likes/likes.service.ts
+++ backend/src/likes/likes.service.ts
@@ -2,12 +2,18 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Like } from './entities/like.entity';
+import { Trip } from '../parcours-sauvegarder/entities/parcours-sauvegarder.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Injectable()
 export class LikesService {
   constructor(
     @InjectRepository(Like)
     private readonly likeRepository: Repository<Like>,
+    @InjectRepository(Trip)
+    private readonly parcourssauvegarderRepository: Repository<Trip>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
   ) {}
 
   async create(createLikeDto: any): Promise<Like[]> {
@@ -51,6 +57,52 @@ export class LikesService {
   update(Id: string, data: any): Promise<any> {
     return this.likeRepository.update(Id, data);
   }
+
+  async updateLike(tripId: string, userId: string, updateLikeDto: any): Promise<Like> {
+    let like = await this.likeRepository.findOne({ where: { trip: { Id: tripId }, user: { Id: userId } } });
+    let isNewLike = false;
+  
+    if (!like) {
+      like = new Like();
+      const user = new User();
+      user.Id = userId;
+      like.user = user;
+
+      const trip = new Trip();
+      trip.Id = tripId;
+      like.trip = trip;
+
+      isNewLike = true;
+    }
+  
+    const trip = await this.parcourssauvegarderRepository.findOne({ where: { Id: tripId } });
+    if (!trip) {
+      throw new NotFoundException(`Trip with ID ${tripId} not found`);
+    }
+  
+    let likeAddedOrRemoved = false;
+    if (updateLikeDto.isLiked) {
+      if (isNewLike) {
+        await this.likeRepository.save(like);
+        trip.LikesNumbers += 1;
+        likeAddedOrRemoved = true;
+      }
+    } else {
+      if (!isNewLike) {
+        await this.likeRepository.remove(like);
+        trip.LikesNumbers -= 1;
+        likeAddedOrRemoved = true;
+      }
+    }
+  
+    if (likeAddedOrRemoved) {
+      await this.parcourssauvegarderRepository.save(trip);
+    }
+  
+    return like;
+  }
+  
+
 
   async remove(Id: string): Promise<any> {
     return await this.likeRepository.delete(Id);
